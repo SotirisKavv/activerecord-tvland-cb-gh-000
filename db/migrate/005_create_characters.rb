c7ab37a373368afbf5661f1ef6a426ec94a250b0
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-21 12:27:52 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,8 +2,7 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
     create_table :characters do |t|
       #primary key of :id is created for us!
-      t.string :first_name
-      t.string :last_name
+      t.string :name
       t.string :catchphrase
     end
   end
